b8d960d4f752c04e25a58cc69f937c99b66fe698
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-08-24 22:11:17 +0600

    degree_script_js

---
+++ FINAL_LAB_TASK_2/degree/script.js
@@ -0,0 +1,23 @@
+function validation()
+{
+	var show = document.getElementById("show");
+	var ssc = document.getElementById("ssc").checked;
+	var hsc = document.getElementById("hsc").checked;
+	var bsc = document.getElementById("bsc").checked;
+	if( ssc == true || hsc == true || bsc == true)
+	{
+		return true;
+	}
+	else
+	{
+		show.innerHTML = "Must be one of them has to be selected";
+		return false;
+	}
+
+}
+
+function clicks()
+{
+	var show = document.getElementById("show");
+	show.innerHTML = "";
+}
