5aefa63b0b993ee3723f26e9f4197e495d951e04
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-09-01 09:11:59 +0600

    email

--- FINAL_LAB_TASK_4/views/registration.php
+++ FINAL_LAB_TASK_4/views/registration.php
@@ -48,7 +48,54 @@
 			document.getElementsByTagName('a')[0].style.display = 'inline';
 		}
 
-		function f2(){
+		function f2()
+		{ 
+			var show = document.getElementById("show");
+	        var e = document.getElementById("email").value;
+	        if( e != "")
+	        {
+		        var a=0;
+		        for( i=0 ; i < e.length ; i++)
+		        {
+			        if(e[i]=='@')
+			        {
+				        a++;
+			        }
+			        	else
+					continue;
+				}
+				if(a==1)
+				{
+					var b = e.split("@");
+					var d = b[1].split(".");
+					var l;
+					for( i=0 ; i < d.length ; i++)
+					{
+						l = d[i];
+					}
+					if( l == "com")
+					{
+					return true;
+					}
+					else
+					{
+						show.innerHTML = "Must be a valid email address";
+						return false;
+					}
+				}
+				else
+				{
+					show.innerHTML = "Must be a valid email address";
+					return false;
+				}
+
+			}
+			else
+			{
+				show.innerHTML = "Cannot be empty";
+				return false;
+			}
+
 			
 		}
 	</script>
