d5b50591f1c411b7540b715377d6a86dd2f63c49
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-09-04 00:29:43 +0600

    reg_email_function

--- FINAL_LAB_TASK_4/views/registration.php
+++ FINAL_LAB_TASK_4/views/registration.php
@@ -2,9 +2,16 @@
 
 	if (isset($_GET['error'])) {
 		
-		if($_GET['error'] == 'db_error'){
+		if($_GET['error'] == 'db_error')
+		{
 			echo "Something went wrong...please try again";
 		}
+		elseif($_GET['error'] == 'id_already_exist')
+		{
+			echo "this id already exist";
+		}
+		else
+			echo "null submission";	
 	}
 
 ?>
@@ -16,86 +23,150 @@
 </head>
 <body>
 
-	<form action="../php/regCheck.php" method="post">
+	<form id="form">
 		<fieldset>
 			<legend>SignUp</legend>
 			<table>
 				<tr>
+					<td>Userid</td>
+					<td><input type="text" id="id" name="id"></td>
+					<td></td>
+				</tr>
+				<tr>
 					<td>Username</td>
-					<td><input type="text" name="username"></td>
+					<td><input type="text" id="username" name="username"></td>
+					<td></td>
 				</tr>
 				<tr>
 					<td>Email</td>
-					<td><input type="text" name="email" onkeyup="f2()"></td>
+					<td><input type="text" id="email" name="email" onkeyup="f1()"></td>
+					<td id="show1"></td>
 				</tr>
 				<tr>
 					<td>Password</td>
-					<td><input type="password" name="password"></td>
+					<td><input type="password" id="password" name="password"></td>
+					<td></td>
 				</tr>
 				<tr>
 					<td></td>
 					<td>
-						<input type="button" name="submit" value="Submit" onclick="f1()">
-						<a href="login.php" style="display: none">login</a>
+						<input type="button" id="submit" name="submit" value="submit" onclick="f2()">
+						<a href="../views/login.php" id="login" style="display: none">Login</a>
 					</td>
+					<td id="show2"></td>
 				</tr>
 			</table>
 		</fieldset>
 	</form>
-
 	<script type="text/javascript">
-		function f1(){
-			document.getElementsByTagName('a')[0].style.display = 'inline';
-		}
-
-		function f2()
-		{ 
-			var show = document.getElementById("show");
-	        var e = document.getElementById("email").value;
-	        if( e != "")
-	        {
-		        var a=0;
-		        for( i=0 ; i < e.length ; i++)
-		        {
-			        if(e[i]=='@')
-			        {
-				        a++;
-			        }
-			        	else
-					continue;
-				}
-				if(a==1)
+		
+		var a = false;
+		var show1 = document.getElementById("show1");
+		function f1()
+		{
+			var email = document.getElementById("email").value;
+			if( email != "")
+			{
+				var count=0;
+				for( i=0 ; i < email.length ; i++)
 				{
-					var b = e.split("@");
-					var d = b[1].split(".");
-					var l;
-					for( i=0 ; i < d.length ; i++)
+					if(email[i]=='@')
 					{
-						l = d[i];
+						count++;
 					}
-					if( l == "com")
+					else
+						continue;
+				}
+				if(count==1)
+				{
+					var b = email.split("@");
+					var c = b[1].split(".");
+					var last;
+					for( i=0 ; i < c.length ; i++)
 					{
-					return true;
+						last = c[i];
+					}
+					if( last == "com" || last == "edu")
+					{
+						var xhttp = new XMLHttpRequest();
+						xhttp.open('POST', '../php/regCheck.php', true);
+						xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+						xhttp.send('email='+email);
+
+						xhttp.onreadystatechange = function ()
+						{
+							if(this.readyState == 4 && this.status == 200)
+							{
+								if(this.responseText != ""){
+									document.getElementById('show1').innerHTML = this.responseText;
+								}
+								else{
+									document.getElementById('show1').innerHTML = "";
+									a = true;
+								}
+								
+							}	
+						}
+
 					}
 					else
 					{
-						show.innerHTML = "Must be a valid email address";
-						return false;
+						show1.innerHTML = "Must be a valid email address";
 					}
 				}
 				else
 				{
-					show.innerHTML = "Must be a valid email address";
-					return false;
+					show1.innerHTML = "Must be a valid email address";
 				}
-
 			}
 			else
 			{
-				show.innerHTML = "Cannot be empty";
-				return false;
+				show1.innerHTML = "Cannot be empty";
 			}
+		}
 
+		function f2()
+		{
+			var show2 = document.getElementById("show2");
+			if(a == true)
+			{
+				var id = document.getElementById('id').value;
+				var username = document.getElementById('username').value;
+				var email = document.getElementById('email').value;
+				var password = document.getElementById('password').value;
+				var submit = document.getElementById('submit').value;
+				if(id!="" && username!="" && password!="")
+				{
+					var xhttp = new XMLHttpRequest();
+					xhttp.open('POST', '../php/regCheck.php', true);
+					xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+					xhttp.send('id='+id+'&username='+username+'&password='+password+'&email='+email+'&submit='+submit);
+
+					xhttp.onreadystatechange = function ()
+					{
+						if(this.readyState == 4 && this.status == 200)
+						{
+							if(this.responseText != "")
+							{
+								document.getElementById('form').reset();
+								document.getElementById('login').style.display = 'inline'
+							}
+							else
+							{
+								document.getElementById('searchdata').innerHTML = "";
+							}	
+						}	
+					}
+				}
+				else
+				{
+					show2.innerHTML = "please fill all the information correctly";
+				}
+			}
+			else
+			{
+				show2.innerHTML = "please fill all the information correctly";
+			}
 			
 		}
 	</script>
