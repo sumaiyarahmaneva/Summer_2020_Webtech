f4e16d52e9a5cc10b4268d3d0c45ccf9f352f483
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-09-06 09:00:44 +0600

    reg_login

---
+++ Final_Lab_Exam/php/logCheck.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+	require_once('../php/session_header.php');
+	require_once('../service/userService.php');
+
+	if(isset($_POST['submit'])){
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		if(empty($username) || empty($password) || empty($status)){
+			header('location: ../views/login.php?error=null_value');
+		}else{
+
+			$user = [
+				'username'=>$username,
+				'password'=>$password,
+				'status'=>$status,
+			];
+			
+			$status = validate($user);
+
+			if($status){
+				$_SESSION['username'] = $username;
+				header('location: ../views/home.php');
+			}else{
+				header('location: ../views/login.php?error=invalid_user');
+			}
+		}
+	}
+
+
+
+?>
