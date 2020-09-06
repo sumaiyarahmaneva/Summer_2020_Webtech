167d10520c1d931a0504ec7f4c6f910dee577738
Sumaiya Rahman Eva <sumaiyarahmaneva.1999@gmail.com>
2020-09-06 08:49:02 +0600

    registration_login

---
+++ FINAL_LAB/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index  Page</title>
+</head>
+<body>
+
+	<h1 id='head2'>Blog First Page</h1>
+	<a href="views/registration.php">Login As Admin</a> |
+	<a href="views/login.php">Login As Author</a>
+</body>
+</html>
